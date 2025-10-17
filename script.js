5fc804bb1c3b6242785a572b8b8fbd1873f642d1
SossaDaDeveloper <mosessossa0@gmail.com>
2025-10-17 13:42:11 +0100

    Profile card task submission

---
+++ script.js
@@ -0,0 +1,11 @@
+const timeElement = document.getElementById("time");
+
+function updateTime() {
+  const now = new Date();
+  const milliseconds = now.getTime();
+  const readable = now.toLocaleTimeString();
+  timeElement.textContent = `${milliseconds} (${readable})`;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
